6ff0a70de7008b58473ec5b693fb9c8d0c93f6fc
ahmedabdsattar <ahmedabdsatar1412000@gmail.com>
2020-04-16 13:28:15 +0200

    Changes

--- admin/Admin_Home_Page.php
+++ admin/Admin_Home_Page.php
@@ -5,16 +5,22 @@
 <!DOCTYPE html>
 <html>
 <head>
- <meta charset="UTF-8">
- <link rel="stylesheet" href="AdminHomePageStyle.css">
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="AdminHomeStyle.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="MenuBarStyle.css">
+  <link rel="index" href="menuBar.html">
 </head>
 
 <body>
 
   <div class = " headerDiv">
     <img src = "<?php echo $_SESSION['image']; ?>" alt="adminphoto">
-    <h3><?php echo $_SESSION['name'];?><h3>
+    <h3><?php echo $_SESSION['name'];?></h3>
   </div>
+  
+
 
          <!-- تم وضع اللينك بنجاح-->
  <div class = "buttonDiv">
--- admin/Shift_Management.php
+++ admin/Shift_Management.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset utf-8>
-<link rel="stylesheet" href="ShiftMangementPageSyle.css">
+<link rel="stylesheet" href="stylecontainerOFShift.css">
 </head>
 <body>
 
